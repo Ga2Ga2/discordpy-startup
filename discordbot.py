43ac3cfd0823db57070c9b105ed748a5426fe4e5
Ga2Ga2 <59717109+Ga2Ga2@users.noreply.github.com>
2020-01-10 12:46:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,26 +6,11 @@ bot = commands.Bot(command_prefix='/')
 token = NjY1MDExNTg3MTk0NjgzNDEy.XhfdGw.SVHjZZsZA4c5oq1brqQjsaFPI0s
 
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-    
-    if message.author.bot:
-        return
-    
-        # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
         
     
 @bot.command()
