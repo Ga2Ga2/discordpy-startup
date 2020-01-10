87ee2bed0a2bee5f38b7a92ed89663764a716463
Ga2Ga2 <59717109+Ga2Ga2@users.noreply.github.com>
2020-01-10 13:34:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,26 @@ async def neko(ctx):
     await ctx.send('にゃーん')
 
 
+from time import sleep
+ 
+target_time = 10
+ 
+def up_timer(secs):
+    for i in range(0,secs):
+        print(i)
+        sleep(1)
+    print("時間です！")
+ 
+ 
+up_timer(target_time)
+
+
+def down_timer(secs):
+    # for i in range(0,secs):から変更
+    for i in range(secs, -1, -1):
+        print(i)
+        sleep(1)
+    print("時間です！")
+    
+    
 bot.run(token)
